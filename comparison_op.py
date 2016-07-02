bfc9aa276a2b00c47be0266389188a92789031f9
Chaitya Shah <chaityacshah@gmail.com>
2016-07-02 13:19:21 +0530

    Applying comparison operators

---
+++ comparison_op.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib as m
+m.use('TkAgg')
+import matplotlib.pyplot as plt
+import quandl
+from matplotlib import style
+style.use('fivethirtyeight')
+
+bridge_height = {'meters':[10.26, 10.31, 10.27, 10.22, 10.23, 6212.42, 10.28, 10.25, 10.31]}
+df = pd.DataFrame(bridge_height)
+
+df['STD'] = pd.rolling_std(df['meters'], 2)
+print(df)
+
+df_std = df.describe()
+print(df_std)
+df_std = df.describe()['meters']['std']
+print(df_std)
+
+df = df[ (df['STD'] < df_std) ]
+print(df)
+
+#df.plot()
+df['meters'].plot()
+plt.show()
